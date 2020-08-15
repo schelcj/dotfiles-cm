7a1dd3044fa215e9328310a519c924e8dc47c93e
Chris Scheller <schelcj@pobox.com>
2020-08-15 15:56:19 -0400

    renamed the script so the aliase is not needed

--- dot_bash/aliases.sh
+++ dot_bash/aliases.sh
@@ -6,7 +6,6 @@ alias music="tmux attach -t music"
 alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias speed_test="wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip"
 alias mux="tmuxinator"
-alias smux="~/scripts/smux.pl"
 alias cm="chezmoi"
 
 alias csnapshot='docker run -it --rm --env HOME=/tmp -v $(pwd):/app -u $(id -u ${USER}):$(id -g ${USER}) carton'
