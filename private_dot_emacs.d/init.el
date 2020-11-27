a21ec70071dcefde033af04dceef7d6097eea30c
Chris Scheller <schelcj@pobox.com>
2020-11-27 15:17:55 -0500

    * added powerline, although it isn't working right yet * getting the babel stuff working for source blocks

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -1,3 +1,9 @@
+;; get rid of that stupid toolbar
+(tool-bar-mode -1)
+
+;; want my line numbers back
+(setq global-linum-mode t)
+
 (require 'package)
 
 ;; Add melpa package source when using package list
@@ -15,7 +21,9 @@
  '(custom-safe-themes
    (quote
     ("2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" default)))
- '(package-selected-packages (quote (deft evil solarized-theme))))
+ '(package-selected-packages
+   (quote
+    (powerline-evil powerline deft evil solarized-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -35,6 +43,17 @@
 (setq org-log-done `time)
 ;; (setq org-log-done `note)
 
+;; load up babel languages
+(org-babel-do-load-languages 'org-babel-load-languages
+    '((sh         . t)
+	(js         . t)
+	(emacs-lisp . t)
+	(perl       . t)
+	(python     . t)
+	(ruby       . t)
+	(css        . t)
+	(plantuml   . t)))
+
 ;; https://jblevins.org/projects/deft/
 ;; this is mainly to give me access to my hundreds of vim-pad notes
 (require 'deft)
@@ -47,5 +66,6 @@
 (setq recentf-max-saved-items 25)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
-;; get rid of that stupid toolbar
-(tool-bar-mode -1)
+;; bring back powerline
+(require 'powerline)
+(powerline-vim-theme)
